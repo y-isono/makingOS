f25143dd200a413477961143a2e6339a23cf1298
yutaro-isono1360 <45115977+YutaroIsono@users.noreply.github.com>
2021-05-05 21:39:31 +0900

    end of day 6

--- bootpack.c
+++ bootpack.c
@@ -8,6 +8,9 @@ void HariMain(void)
 	int mx, my;
 
 	init_gdtidt();
+	init_pic();
+	io_sti(); /* IDT/PICの初期化が終わったのでCPUの割り込み禁止を解除 */
+
 	init_palette();
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
     
@@ -18,6 +21,9 @@ void HariMain(void)
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+    io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
     // static char font_A[16] = {
 	// 	0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
 	// 	0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
