e361ac18da81c0324110c3d3c374a2a77b71565f
yutaro-isono1360 <45115977+YutaroIsono@users.noreply.github.com>
2021-05-05 21:08:38 +0900

    introduce bootpack.h

--- graphic.c
+++ graphic.c
@@ -1,40 +1,5 @@
+#include "bootpack.h"
 /* グラフィック処理関係 */
-
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
-void stripefill(unsigned char *vram);
-void triboxfill8(vram);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen8(char *vram, int x, int y);
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize,
-	int pysize, int px0, int py0, char *buf, int bxsize);
-
-#define COL8_000000		0
-#define COL8_FF0000		1
-#define COL8_00FF00		2
-#define COL8_FFFF00		3
-#define COL8_0000FF		4
-#define COL8_FF00FF		5
-#define COL8_00FFFF		6
-#define COL8_FFFFFF		7
-#define COL8_C6C6C6		8
-#define COL8_840000		9
-#define COL8_008400		10
-#define COL8_848400		11
-#define COL8_000084		12
-#define COL8_840084		13
-#define COL8_008484		14
-#define COL8_848484		15
-
 void init_palette(void)
 {
 	static unsigned char table_rgb[16 * 3] = {
@@ -203,7 +168,7 @@ void stripefill(unsigned char *vram)
     return;
 };
 
-void triboxfill8(vram)
+void triboxfill8(unsigned char *vram)
 {
     boxfill8(vram, 320, COL8_FF0000,  20,  20, 120, 120);
 	boxfill8(vram, 320, COL8_00FF00,  70,  50, 170, 150);
