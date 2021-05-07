2e5c32ec2b6e52f767e344739c2fa4107e1b6515
yutaro-isono1360 <45115977+YutaroIsono@users.noreply.github.com>
2021-05-07 21:56:59 +0900

    end of day 7 receiving infomation from mouce

--- bootpack.h
+++ bootpack.h
@@ -12,6 +12,8 @@ struct BOOTINFO { /* 0x0ff0-0x0fff */
 /* naskfunc.nas */
 void io_hlt(void);
 void io_cli(void);
+void io_sti(void);
+int io_in8(int port);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
@@ -20,6 +22,16 @@ void load_idtr(int limit, int addr);
 void asm_inthandler21(void);
 void asm_inthandler27(void);
 void asm_inthandler2c(void);
+
+/* fifo.c */
+struct FIFO8 {
+	unsigned char *buf;
+	int p, q, size, free, flags;
+};
+void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf);
+int fifo8_put(struct FIFO8 *fifo, unsigned char data);
+int fifo8_get(struct FIFO8 *fifo);
+int fifo8_status(struct FIFO8 *fifo);
 
 /* graphic.c */
 void init_palette(void);
@@ -75,6 +87,10 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define AR_INTGATE32	0x008e
 
 /* int.c */
+struct KEYBUF {
+	unsigned char data[32];
+	int next_r, next_w, len;
+};
 void init_pic(void);
 void inthandler21(int *esp);
 void inthandler27(int *esp);
