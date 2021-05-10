bd105c05932f3258ea59904ece244a7f4aaae7fc
yutaro-isono1360 <45115977+YutaroIsono@users.noreply.github.com>
2021-05-10 22:00:31 +0900

    timer speeding up (2)

--- timer.c
+++ timer.c
@@ -7,37 +7,77 @@
 
 struct TIMERCTL timerctl;
 
+#define TIMER_FLAGS_ALLOC		1	/* 確保した状態 */
+#define TIMER_FLAGS_USING		2	/* タイマ作動中 */
+
 void init_pit(void)
 {
+	int i;
 	io_out8(PIT_CTRL, 0x34);
 	io_out8(PIT_CNT0, 0x9c);
 	io_out8(PIT_CNT0, 0x2e);
 	timerctl.count = 0;
-    timerctl.timeout = 0;
+	timerctl.next = 0xffffffff; /* 最初は作動中のタイマがないので */
+	for (i = 0; i < MAX_TIMER; i++) {
+		timerctl.timer[i].flags = 0; /* 未使用 */
+	}
+	return;
+}
+
+struct TIMER *timer_alloc(void)
+{
+	int i;
+	for (i = 0; i < MAX_TIMER; i++) {
+		if (timerctl.timer[i].flags == 0) {
+			timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
+			return &timerctl.timer[i];
+		}
+	}
+	return 0; /* 見つからなかった */
+}
+
+void timer_free(struct TIMER *timer)
+{
+	timer->flags = 0; /* 未使用 */
+	return;
+}
+
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data)
+{
+	timer->fifo = fifo;
+	timer->data = data;
+	return;
+}
+
+void timer_settime(struct TIMER *timer, unsigned int timeout)
+{
+	timer->timeout = timeout + timerctl.count;
+	timer->flags = TIMER_FLAGS_USING;
 	return;
 }
 
 void inthandler20(int *esp)
 {
+	int i;
 	io_out8(PIC0_OCW2, 0x60);	/* IRQ-00受付完了をPICに通知 */
 	timerctl.count++;
-	if (timerctl.timeout > 0) { /* タイムアウトの設定 */
-        timerctl.timeout--;
-		if (timerctl.timeout == 0) {
-			fifo8_put(timerctl.fifo, timerctl.data);
+	if (timerctl.next > timerctl.count) {
+		return; /* まだ次の時刻になってないので、もうおしまい */
+	}
+	timerctl.next = 0xffffffff;
+	for (i = 0; i < MAX_TIMER; i++) {
+		if (timerctl.timer[i].flags == TIMER_FLAGS_USING) {
+			if (timerctl.timer[i].timeout <= timerctl.count) {
+				/* タイムアウト */
+				timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
+				fifo8_put(timerctl.timer[i].fifo, timerctl.timer[i].data);
+			} else {
+				/* まだタイムアウトではない */
+				if (timerctl.next > timerctl.timer[i].timeout) {
+					timerctl.next = timerctl.timer[i].timeout;
+				}
+			}
 		}
-    }
-	return;
-}
-
-void settimer(unsigned int timeout, struct FIFO8 *fifo, unsigned char data)
-{
-	int eflags;
-	eflags = io_load_eflags();
-	io_cli();
-	timerctl.timeout = timeout;
-	timerctl.fifo = fifo;
-	timerctl.data = data;
-	io_store_eflags(eflags);
+	}
 	return;
 }
